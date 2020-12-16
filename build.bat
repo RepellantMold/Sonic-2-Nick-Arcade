dc36cde1168ea987ceb993ec753b9643e3393329
Jacob A <76079717+RepellantMold@users.noreply.github.com>
2020-12-16 14:04:12 -0800

    Add files via upload

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
 IF EXIST s2built.bin move /Y s2built.bin s2built.prev.bin >NUL
-asm68k /k /p /o ae- s2.asm, s2built.bin , , s2.lst >s2.log
+asm68k /k /m /c /p /o ae- s2.asm, s2built.bin , , s2.lst >s2.log
