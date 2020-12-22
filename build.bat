67dd30e4112140ffdf50ef1192e4e8b4f5968868
Jacob T. Andersen <jaketehspartansalt@gmail.com>
2020-12-21 20:48:12 -0800

    Add files via upload

--- build.bat
+++ build.bat
@@ -1,19 +1,20 @@
 @echo off
 echo Making s2built.prev.bin...
 IF EXIST s2built.bin move /Y s2built.bin s2built.prev.bin >NUL
-echo Building the ROM....
-cd mappings
-cd 128x128
-kc-compress "GHZ (Uncompressed).bin" GHZ.bin
+echo Done.
+cls
+cd mappings/128x128
+kc-compress.exe "GHZ (Uncompressed).bin" GHZ.bin
 cd..
 cd..
-asm68k /k /m /c /q /p /o ae- s2.asm, s2built.bin , , s2.lst >s2.log
+echo Building the ROM...
+asm68k /c /q /p /o ws+ /o l+ /o ae- s2.asm, s2built.bin , , s2.lst >error.log
 cls
 IF NOT EXIST s2built.bin goto LABLERR
 echo Build successful!
 goto LABLDONE
 :LABLERR
-echo Build failed.
-type s2.log
+echo Build failed, look below for why.
+type error.log
 :LABLDONE
 pause
